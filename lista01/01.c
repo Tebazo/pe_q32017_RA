1f163087cdbe0dbdb38253d0a2b456794f81d548
Tebazo <mariocairespereira@gmail.com>
2017-09-25 08:56:11 -0300

    Adicionando exc 1

---
+++ lista01/01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+int a;
+
+printf("Digite o número desejado: ");
+scanf("%d", &a);
+
+if(a%2 == 0){
+	printf("O número é par\n");
+}else printf("O número é ímpar\n");
+
+
+
+}
