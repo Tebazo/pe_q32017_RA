5992fdd8c8aaff52441dbc9e34032808afbfe49a
Tebazo <mariocairespereira@gmail.com>
2017-09-25 09:08:55 -0300

    Add exc 3

---
+++ lista01/03.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+int a,b,sub;
+
+printf(" Digite o primeiro número: " );
+scanf("%d", &a); 
+printf(" Digite o segundo número número: ");
+scanf("%d", &b);
+
+sub = a - b;
+
+printf("A sub é: %d\n", sub);
+
+}
